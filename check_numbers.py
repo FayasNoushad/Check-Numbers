4a718804d17db9e38688913ad1318329caa311e8
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-11-18 17:22:34 +0530

    Add some definitions (#1)
    - Add check positive numbers
    - Add check negative numbers
    - Add check zero

---
+++ check_numbers.py
@@ -0,0 +1,19 @@
+def is_positive(num):
+    if int(num) > 0:
+        return True
+    else:
+        return False
+
+
+def is_negative(num):
+    if int(num) < 0:
+        return True
+    else:
+        return False
+
+
+def is_zero(num):
+    if int(num) == 0:
+        return True
+    else:
+        return False
